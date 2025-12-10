0b6b001f48ab2c93e3866c78f7f696903f270660
Mahesh Prayaga <maheshprayaga135@gmail.com>
2025-12-10 10:22:49 +0530

    Initial Next.js + Tailwind boilerplate

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen p-10">
+      <h1 className="text-4xl font-bold">GIT-A-THON Website</h1>
+      <p className="text-gray-400 mt-4">
+        Boilerplate ready. Component development can start.
+      </p>
+    </main>
+  );
+}
